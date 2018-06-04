24e5f732303707cafbdc55e9aa7acefec12b4e84
Andy Tang <kiwikefei@gmail.com>
2018-06-04 22:23:03 +1200

    l8: orm

--- mysite/code/HomePage.php
+++ mysite/code/HomePage.php
@@ -8,4 +8,9 @@ class HomePage extends Page
 class HomePage_Controller extends Page_Controller
 {
 
+    public function LatestArticles($count = 2)
+    {
+        $articles = ArticlePage::get()->sort('Created', 'DESC')->limit($count);
+        return $articles;
+    }
 }
