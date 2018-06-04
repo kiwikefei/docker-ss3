1317e429405f43a2465f0fa886ae08b514f5a2ed
Andy Tang <kiwikefei@gmail.com>
2018-06-04 20:52:58 +1200

    l5: holder page and static=yml

---
+++ mysite/code/ArticleHolder.php
@@ -0,0 +1,19 @@
+<?php
+
+class ArticleHolder extends Page
+{
+    // in mysite/_config/config.yml
+    // ArticleHolder:
+    //   allowed_children: ArticlePage
+    //   can_be_root: true
+    // private static variable is identical to yml, however yml has higher priority.
+
+    private static $allowed_children = [ 'ArticlePage' ];
+    private static $can_be_root = false;
+}
+
+class ArticleHolder_Controller extends Page_Controller
+{
+
+
+}
---
+++ mysite/code/ArticlePage.php
@@ -0,0 +1,10 @@
+<?php
+class ArticlePage extends Page
+{
+
+}
+
+class ArticlePage_Controller extends Page_Controller
+{
+
+}
